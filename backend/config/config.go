b2d3cf9c1edac222dbefb9fb5811921cf22cec04
marchelrn <marchelmanullang026@student.unsrat.ac.id>
2026-05-24 23:11:33 +0800

    feat: adding remote database testing configuration

--- backend/config/config.go
+++ backend/config/config.go
@@ -28,25 +28,29 @@ func Load() {
 	}
 
 	env := os.Getenv("ENV")
-	if env == "" || (env != "development" && env != "production" && env != "testing") {
+	if env == "" || (env != "production" && env != "testing" && env != "remote_testing") {
 		env = "development"
 	}
 
 	config = &Config{
-		Port:      getEnv("PORT", "3030"),
+		Port:      getEnv("PORT", "PORT_BACKUP"),
 		Env:       env,
 		DBUrl:     buildDbUrl(env),
 		JWTSecret: os.Getenv("JWT_SECRET"),
 	}
 
 	if config.Port == "" {
-		config.Port = "3030"
+		config.Port = getEnv("PORT_BACKUP", "3030")
 	}
 }
 
 func buildDbUrl(env string) string {
 	if env == "production" {
 		return os.Getenv("DB_URL")
+	}
+
+	if env == "remote_testing" {
+		return os.Getenv("DB_URL_testing")
 	}
 
 	dbUser := os.Getenv("DB_USER")
